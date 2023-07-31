388fcb710ba1fc71971e839566806c3de053d172
claxxz <141022968+claxxz@users.noreply.github.com>
2023-07-31 11:50:19 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xf33378671242c9ae4199b11c3101e7871a5cde7c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
